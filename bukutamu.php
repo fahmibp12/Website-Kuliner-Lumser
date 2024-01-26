ef68e33f7bd9c70d1fc3494744ada9452a88ee2e
fahmibp12 <fahmibudiprasetya@gmail.com>
2024-01-26 15:20:14 +0700

    first commit

---
+++ bukutamu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buku Tamu</title>
+    <link rel="stylesheet" href="bukutamu.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="Header">
+        <h1>LUMSER</h1>
+    </div>
+
+    <div class="navigasi">
+        <ul>
+            <li><a href="Home.html">Home</a></li>
+            <li><a href="Blogku.html">About</a></li>
+            <li><a href="index.html">Blog</a></li>
+            <li><a href="bukutamu.php">Buku Tamu</a></li>
+        </ul>
+    </div>
+
+    <div class="container">
+        <form action="Home.html" method="post">
+            <h2>BUKU TAMU</h2>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name"><br>
+
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email"><br>
+
+            <label>Komentar</label>
+            <input type="text" name="komentar" placeholder="Komentar"><br>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+
+    <div class="footercontainer">
+        <div class="footer">
+            <ul>
+                <li><a href="https://www.instagram.com/fahmi_bp_?igsh=MXdvNm95MTcxcmpoeQ==">Instagram</a></li>
+                <li><a href="https://www.tiktok.com/@readmore_12?_t=8inW73Sz3Nh&_r=1">Tiktok</a></li>
+                <li><a href="https://www.threads.net/@fahmi_bp_">Threads</a></li>
+            </ul>
+        </div>
+    </div>
+
+        <div class="footerbottom">
+            <p>Copyright &copy;2024; Designed by <span class="designer">FahmiBp</span></p>
+        </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kuliner Nusantara</title>
+    <link rel="stylesheet" type="text/css"href="php.css">
+</head>
+<body>
+    <form action="Home.html" method="post">
+        <h2>LOGIN</h2>
+        <label>Username</label>
+        <input type="text" name="uname" placeholder="Username"><br>
+
+        <label>Password</label>
+        <input type="Password" name="Password" placeholder="Password"><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
